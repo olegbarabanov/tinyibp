a066ee396e8d8f65c1fa641a60ed9ef455054ec2
Barabanov Oleg <mail@olegbarabanov.ru>
2021-02-23 13:40:27 +0500

    Дополнен тест BlurFilter

--- src/components/__test__/BlurFilter.spec.ts
+++ src/components/__test__/BlurFilter.spec.ts
@@ -1,17 +1,28 @@
 /* eslint-disable node/no-unpublished-import */
-import {createLocalVue, mount} from '@vue/test-utils';
+import {createLocalVue, mount, shallowMount} from '@vue/test-utils';
 import BlurFilter from '@/components/BlurFilter.vue';
-import BootstrapVue from 'bootstrap-vue';
+import BootstrapVue, {BFormInput} from 'bootstrap-vue';
 
 describe('BlurFilter.vue', () => {
-  test('Передает пустые значения в настройках', () => {
+  test('Передает пустые значения в настройках', async () => {
     const localVue = createLocalVue();
     localVue.use(BootstrapVue);
     const wrapper = mount(BlurFilter, {
       localVue,
     });
+    const input = wrapper.findComponent(BFormInput);
+    input.setValue('50');
+    expect(wrapper.emitted()['update:level']).toBeTruthy();
+    expect(wrapper.emitted()['update:level']?.[0]).toEqual([50]);
+    input.setValue(0);
+    expect(wrapper.emitted()['update:level']).toBeTruthy();
+    expect(wrapper.emitted()['update:level']?.[1]).toEqual([0]);
 
-    const input = wrapper.find('b-form-input');
-    expect(input.vm.$data.value).toBe('100');
+    wrapper.setProps({level: 100});
+    await wrapper.vm.$nextTick();
+    console.log('value=>', input.vm.$props.value);
+    expect(input.vm.$props.value).toEqual(100);
+
+    wrapper.destroy();
   });
 });
