ffdb6425d8b97c250f7f803a0d45c89bc5edd061
Barabanov Oleg <mail@olegbarabanov.ru>
2021-02-28 20:03:07 +0500

    commented code removed

--- src/App.vue
+++ src/App.vue
@@ -130,18 +130,6 @@ export default Vue.extend({
     },
   },
 });
-
-/*
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-*/
 </script>
 
 <style>
