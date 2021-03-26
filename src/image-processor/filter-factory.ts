53cc9a9e1d4d7658ec79b5c63fd7e0d79dcc010c
Barabanov Oleg <mail@olegbarabanov.ru>
2021-03-26 09:40:41 +0500

    add comments

--- src/image-processor/filter-factory.ts
+++ src/image-processor/filter-factory.ts
@@ -1,12 +1,24 @@
 import {FilterConstructor, FilterInterface} from '.';
 
+/**
+ * Factory class for filters
+ */
 export class FilterFactory {
   protected filterCollection: Array<FilterConstructor> = [];
 
+  /**
+   * method for registering filters for subsequent work with it
+   * @param filter - a class (not an object!) that implements the FilterInterface
+   */
   registerFilter(filter: FilterConstructor): void {
     this.filterCollection.push(filter);
   }
 
+  /**
+   * method for searching for the class corresponding to the filter name
+   * @param filterName - string name of the filter
+   * @returns corresponding class or null
+   */
   findFilter(filterName: string): FilterConstructor | null {
     return (
       this.filterCollection.find(
@@ -15,10 +27,19 @@ export class FilterFactory {
     );
   }
 
+  /**
+   * method that returns a list of registered filters
+   * @returns a list of registered filters
+   */
   getFilterCollection(): Array<FilterConstructor> {
     return this.filterCollection;
   }
 
+  /**
+   * method that returns an instance of the filter
+   * @param filterName - string name of the filter
+   * @returns - instance of the filter class or null
+   */
   createInstance(filterName: string): FilterInterface | null {
     const filter = this.findFilter(filterName);
     return filter ? new filter() : null;
