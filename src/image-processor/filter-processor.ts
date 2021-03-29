e1cfa121bd98d94f936d8fa2e6d7782ab1ea95d2
Barabanov Oleg <mail@olegbarabanov.ru>
2021-03-29 10:27:57 +0500

    added a description of the methods and class

--- src/image-processor/filter-processor.ts
+++ src/image-processor/filter-processor.ts
@@ -1,16 +1,32 @@
 import {FilterFactory, FilterMap} from '.';
 
+/**
+ * Сlass for registering and executing filters
+ */
 export default class FilterProcessor {
   protected filterFactory: FilterFactory;
 
+  /**
+   * @param filterFactory - filter factory class or undefined.
+   */
   constructor(filterFactory?: FilterFactory) {
     this.filterFactory = !filterFactory ? new FilterFactory() : filterFactory;
   }
 
+  /**
+   * Кeturns the assigned class factory
+   * @returns - assigned object implements the FilterFactory class
+   */
   getFilterFactory(): FilterFactory {
     return this.filterFactory;
   }
 
+  /**
+   * A method that starts processing the canvas with filters based on filter maps
+   * @param canvas - object implements the Offscreen Canvas class
+   * @param filterMaps - a filter maps that describes the filter parameters
+   * @returns - Promise which returns a new OffscreenCanvas
+   */
   async run(
     canvas: OffscreenCanvas,
     filterMaps: Array<FilterMap>
