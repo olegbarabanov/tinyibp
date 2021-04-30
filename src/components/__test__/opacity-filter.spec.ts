07ed6c712b6dc0173897ad28ea8ae58cf59c3e10
Barabanov Oleg <mail@olegbarabanov.ru>
2021-04-30 19:47:12 +0500

    update tests

--- src/components/__test__/opacity-filter.spec.ts
+++ src/components/__test__/opacity-filter.spec.ts
@@ -1,12 +1,30 @@
 /* eslint-disable node/no-unpublished-import */
-import {createLocalVue, mount, shallowMount} from '@vue/test-utils';
+import {createLocalVue, mount} from '@vue/test-utils';
 import OpacityFilter from '@/components/opacity-filter.vue';
-import BootstrapVue from 'bootstrap-vue';
+import BootstrapVue, {BFormInput} from 'bootstrap-vue';
 
 describe('opacity-filter.vue', () => {
-  /*
-    EMPTY TEST !
-    MUST BE CREATED FOR RELEASE !
-  */
-  test('empty test', async () => {});
+  test('emits the correct event with a numeric value when different "level" types are set.', async () => {
+    const localVue = createLocalVue();
+    localVue.use(BootstrapVue);
+    const wrapper = mount(OpacityFilter, {
+      localVue,
+      mocks: {
+        $t: (msg: string) => msg,
+        $tc: (msg: string) => msg,
+      },
+    });
+
+    const input = wrapper.findComponent(BFormInput);
+    expect(input.exists()).toBe(true);
+    input.setValue(0);
+    expect(wrapper.emitted()['update:level']?.[0]).toBeTruthy();
+    expect(wrapper.emitted()['update:level']?.[0]).toEqual([0]);
+    input.setValue('50.5');
+    expect(wrapper.emitted()['update:level']?.[1]).toBeTruthy();
+    expect(wrapper.emitted()['update:level']?.[1]).toEqual([50.5]);
+    input.setValue(100);
+    expect(wrapper.emitted()['update:level']?.[2]).toBeTruthy();
+    expect(wrapper.emitted()['update:level']?.[2]).toEqual([100]);
+  });
 });
